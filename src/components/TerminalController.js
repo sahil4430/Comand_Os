64b57c40ed90438a1f008e5eb916f7c86de5021a
Sahil4430 <panwarsahil204@gmail.com>
2025-07-01 12:20:57 +0530

    elctron set

---
+++ src/components/TerminalController.js
@@ -0,0 +1,33 @@
+'use client';
+
+import React, { useState } from "react";
+
+import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+
+function TerminalController() {
+  const [terminalLineData, setTerminalLineData] = useState([
+    <TerminalOutput key={0}>Welcome to the Comand Os Terminal !</TerminalOutput>,
+  ]);
+
+  return (
+    <div className="container">
+      {/* <Terminal
+        name="Comand OS"
+        colorMode={"Dark"}
+        onInput={(terminalInput) => {
+          console.log(`New terminal input received: '${terminalInput}'`);
+
+          setTerminalLineData(prev => [
+            ...prev,
+            <TerminalOutput key={prev.length}>{`> ${terminalInput}`}</TerminalOutput>,
+          ]);
+        }}
+      >
+        {terminalLineData}
+      </Terminal> */}
+    </div>
+
+  );
+}
+
+export default TerminalController;
---
+++ src/electron/main.js
@@ -0,0 +1,21 @@
+import { BrowserWindow, app } from "electron";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+
+app.on("ready", () => {
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    const indexPath = path.join(__dirname, "..", "..", "dist-react", "index.html");
+
+
+    mainWindow.loadFile(path.join(indexPath));
+
+})
