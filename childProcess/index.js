874a3ec1e1778a924f4f6bf0eebb85ebba7886cf
Sahil4430 <panwarsahil204@gmail.com>
2025-07-16 15:26:42 +0530

    searching using the chil_process

--- childProcess/index.js
+++ childProcess/index.js
@@ -3,9 +3,10 @@ import { spawn } from 'node:child_process';
 
 // code to get list of files in /usr directory
 // using spawn method of child_process module
-const ls = spawn( 'ls', ['-lh', '/usr']);
+
+const ls = spawn( 'ls', ['-lh', '/Users/sahilpanwar/Development/web/comand_os/childProcess']);
 ls.stdout.on('data',(data)=>{
-    console.log(`stdout:${data}`)
+    console.log(`stdout in kb:${data}`)
 });
 
 ls.stderr.on('data' , (data)=>{
@@ -15,4 +16,40 @@ ls.stderr.on('data' , (data)=>{
 ls.on('close',(code)=>{
     console.log(`child process exited with code ${code}`);
 });
+
+
+// code to get list of processes running on the system
+// const ps = spawn('ps',['ax']);
+// const grep = spawn('grep',['ssh']);
+
+// ps.stdout.on('data',(data)=>{
+//     grep.stdin.write(data);
+// });
+//  ps.stderr.on('data',(data)=>{
+//     Console.log(`ps stderr: ${data}`);
+//  })
+
+//  ps.on('close',(code)=>{
+//     if(code !==0){
+//         console.log(`ps process exited with code ${code}`)
+    
+//     }
+//     grep.stdin.end();
+//  });
+
+//  grep.stdout.on('data',(data)=>{
+//     console.log(data.toString());
+
+//  });
+
+//  grep.stderr.on('data',(data)=>{
+//     console.log(`error:${data}`);
+//  });
+
+//  grep.on('close',(code)=>{
+//     if(code!==0){
+//         console.log(`grep process exited with code ${code}`);
+//     }
+//  })
+
 
---
+++ childProcess/txtSearch.js
@@ -0,0 +1,38 @@
+import {spawn} from 'child_process';
+// const dirname = 'web';
+// const find = spawn('find',['/', '-type', 'd','-name', dirname]);
+// const find = spawn('find', ['/Users', '/Applications', '/Volumes'], ['-type', 'd', '-iname', '*web*']);
+
+// find.stdout.on('data',(data)=>{
+//     console.log(data.toString());
+// });
+// find.stderr.on('data',(data)=>{
+//     console.error(`AError:${data.toString()}`)
+// });
+// find.on('close',(code)=>{
+//     console.log(`childprossess exited with ${code}`)
+// });
+const find = spawn('find', [
+  '/Users',
+  '/Applications',
+  '/Volumes',
+  '-type',
+  'd',
+  '-iname',
+  '*nexttttttttttttttt$*'
+]);
+
+find.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+
+find.stderr.on('data', (data) => {
+  const msg = data.toString();
+  if (!msg.includes('Operation not permitted') && !msg.includes('Permission denied')) {
+    console.error(`AError: ${msg}`);
+  }
+});
+
+find.on('close', (code) => {
+  console.log(`childprocess exited with code ${code}`);
+});
