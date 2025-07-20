253435170149a9a8803f2b6a8565a93603e448e2
Sahil4430 <panwarsahil204@gmail.com>
2025-07-20 22:24:50 +0530

    working for all os  but only for 1 word application

--- childProcess/open.js
+++ childProcess/open.js
@@ -1,9 +1,49 @@
 import {spawn} from 'child_process';
+import os from 'node:os';
 
-const app = spawn ('open',['-a', 'Freeform']);
- app.stdout.on('data',(data)=>{
-    console.log(`Stdout: ${data}`)
- });
-  app.stderr.on( 'data',(data)=>{
-    console.log (`stderr: ${data}`)
-  })
+// only for macOS
+// const app = spawn ('open',['-a', 'Freeform']);
+//  app.stdout.on('data',(data)=>{
+//     console.log(`Stdout: ${data}`)
+//  });
+//   app.stderr.on( 'data',(data)=>{
+//     console.log (`stderr: ${data}`)
+//   })
+
+// for all OS we using diff aproach
+
+ function launchApp(appName , filePath = null){
+    const platform  = os.platform();
+    let command;
+    let arg=[];
+    switch(platform){
+        case 'darwin':
+            command ='open';
+            arg =['-a', appName ];
+            if ( filePath){
+                arg.push (filePath);}
+            break;
+            case 'win32':
+                command ='cmd';
+                arg =[ '/c', 'start', '',`${appName}`];
+                if(filePath) arg.push(filePath);
+                break;
+            case 'linux':
+            command = appName.tolowerCase();
+            if (filePath) arg.push(filePath);
+            break;
+             default:
+                console.log( "unsupported platform");
+                return;
+         }
+
+         const app = spawn( command , arg ,{shell : true});
+         app.stdout.on('data', (data ) => {
+            console.log(`Stdout: ${data}`);
+         });
+         app.stderr.on('data',(data) =>{
+            console.log(`stderrr : ${data}`)
+         });}
+
+         launchApp('System Settings');
+
